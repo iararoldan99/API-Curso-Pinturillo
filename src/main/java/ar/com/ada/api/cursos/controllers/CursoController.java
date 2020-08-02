da74b10f568b8d45ef1def8ad094ce9f737d4e8c
iararoldan99 <iarah_debera@hotmail.com>
2020-08-02 10:47:06 -0300

    Importando

--- src/main/java/ar/com/ada/api/cursos/controllers/CursoController.java
+++ src/main/java/ar/com/ada/api/cursos/controllers/CursoController.java
@@ -2,15 +2,19 @@ package ar.com.ada.api.cursos.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ar.com.ada.api.cursos.services.CursoService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import ar.com.ada.api.cursos.entities.*;
+import ar.com.ada.api.cursos.models.request.CursoAsigDocRequest;
 import ar.com.ada.api.cursos.models.request.CursoRequest;
 import ar.com.ada.api.cursos.models.response.GenericResponse;
 
