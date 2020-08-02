d9c6d73bd74d6b8eb54f376e3351ad1d0764eadf
iararoldan99 <iarah_debera@hotmail.com>
2020-08-02 10:45:12 -0300

    Creamos el webmethod para asignar un docente a un curso. Creamos el metodo en el service, checkeando si el docente no existe en el curso y si es asi, lo asignamos. Modificamos el webmethod listarCursos para que soporte que nos devuelva una lista de cursos sin docentes a traves de un RequestParam. y creamos el listarCusrosSinDocentes que recorre la lista de docentes y chequea por aquellos que no tengan docentes.

--- src/main/java/ar/com/ada/api/cursos/controllers/CursoController.java
+++ src/main/java/ar/com/ada/api/cursos/controllers/CursoController.java
@@ -17,6 +17,43 @@ import ar.com.ada.api.cursos.models.response.GenericResponse;
 @RestController
 public class CursoController {
 
+   // sin filtro: /api/cursos
+  // con filtro sin docentes: /api/cursos?sinDocentes=true
+  // /api/cursos?docentes=null
+  // sinDocentes es un queryParam que nos permite FILTRAR
+  // front si envia ese parametro con el valor true, filtramos.
+  @GetMapping("/api/cursos")
+  public ResponseEntity<List<Curso>> listaCursos(@RequestParam(value = "sinDocentes", required = false) boolean sinDocentes) {
+    List<Curso> listaCursos = new ArrayList<>();
+    if (sinDocentes) {
+      // listaCursos = algo que nos devuelva la llista sin docentes.
+      listaCursos = cursoService.listaCursosSinDocentes();
+    } else {
+      listaCursos = cursoService.listaCursos();
+    }
+    return ResponseEntity.ok(listaCursos);
+  }
+
+   // - Asignar Docente a un curso.
+  // /api/cursos/docentes/25 : este representaria al id del docente
+  // /api/cursos/25/docentes: este prepresentaria al id del curso.
+  @PostMapping("/api/cursos/{cursoId}/docentes")
+  public ResponseEntity<GenericResponse> asignarDocente(@PathVariable Integer cursoId,
+      @RequestBody CursoAsigDocRequest cADR) {
+    GenericResponse gR = new GenericResponse();
+    if (cursoService.asignarDocente(cursoId, cADR.docenteId)) {
+
+      gR.isOk = true;
+      gR.message = "Docente asignado con éxito al Curso";
+      return ResponseEntity.ok(gR);
+    }
+    gR.isOk = false;
+    gR.message = "El Docente no pudo ser asignado.";
+    return ResponseEntity.badRequest().body(gR);
+
+
+  }
+
     @Autowired
     CursoService cursoService;
     @PostMapping("/api/cursos")
