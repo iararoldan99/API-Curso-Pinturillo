e3b81f7915888e228fd2ef9a19453a70c35799ba
IaraRoldan99 <iarah_debera@hotmail.com>
2020-10-02 21:55:52 -0300

    Se mueven todas las configuraciones al app.properties.

--- src/main/java/ar/com/ada/api/cursos/sistema/pagada/PagADAService.java
+++ src/main/java/ar/com/ada/api/cursos/sistema/pagada/PagADAService.java
@@ -1,5 +1,6 @@
 package ar.com.ada.api.cursos.sistema.pagada;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ar.com.ada.api.cursos.entities.Inscripcion;
 import ar.com.ada.api.cursos.sistema.pagada.models.ResultadoCreacionDeuda;
@@ -12,9 +13,17 @@ import kong.unirest.json.JSONObject;
 import java.util.*;
 import java.text.SimpleDateFormat;
 
-
 @Service
 public class PagADAService {
+
+    @Value("${pagADA.apiKey}")
+    private String apiKey;
+    @Value("${pagADA.apiBaseUri}")
+    public String apiBaseUri;
+    @Value("${pagADA.empresaId}")
+    public Integer empresaId;
+    @Value("${pagADA.tipoServicioId}")
+    public Integer tipoServicioId;
 
     public Integer crearDeudor(Estudiante estudiante) {
 
@@ -50,9 +59,10 @@ public class PagADAService {
         ResultadoCreacionDeuda resultado = new ResultadoCreacionDeuda();
 
         JsonNode r;
-        HttpResponse<JsonNode> request = Unirest.post("https://pagada.herokuapp.com/api/deudores")
+
+        HttpResponse<JsonNode> request = Unirest.post(this.apiBaseUri + "/deudores")
                 .header("content-type", "application/json").body(deudor) // AcaPasamos el RequestBody
-                .header("api", "831DYEY1811NOMECORTENELSERVICIO").asJson();
+                .header("api", this.apiKey).asJson();
 
         r = request.getBody();
 
@@ -69,7 +79,8 @@ public class PagADAService {
     public Integer crearDeuda(Inscripcion inscripcion) {
         Servicio servicio = new Servicio();
 
-        servicio.empresaId = 4; // ID de empresa en PagADA NO TIENE QUE ESTAR HARCODEADO
+        servicio.empresaId = this.empresaId; // ID de empresa en PagADA NO TIENE QUE ESTAR HARCODEADO
+        servicio.tipoServicioId = this.tipoServicioId; // EDUCACION
         servicio.tipoServicioId = 13; // EDUCACION
         servicio.deudorId = inscripcion.getUsuario().getEstudiante().getPagADA_deudorId();
         servicio.tipoComprobanteId = "SERVICIO";
@@ -113,10 +124,9 @@ public class PagADAService {
         infoServicioJson.put("fechaVencimiento", formatter.format(servicio.fechaVencimiento));
 
         JsonNode r;
-        HttpResponse<JsonNode> request = Unirest.post("https://pagada.herokuapp.com/api/servicios")
+        HttpResponse<JsonNode> request = Unirest.post(this.apiBaseUri + "/servicios")
                 .header("content-type", "application/json").body(infoServicioJson) // AcaPasamos el RequestBody
-                .header("api", "831DYEY1811NOMECORTENELSERVICIO").asJson(); // QUE TAMPOCO HAY QUE HAARCODEARLA
-
+                .header("api", this.apiKey).asJson(); // QUE TAMPOCO HAY QUE HAARCODEARLA
         r = request.getBody();
 
         JSONObject jsonObject = r.getObject();
@@ -132,12 +142,13 @@ public class PagADAService {
     public Integer pagarServicio(Inscripcion inscripcion, String medioPago, String infoMedioPago) {
 
         Pago pago = new Pago();
-        //Mapear los datos de la inscripcion al pago
+        // Mapear los datos de la inscripcion al pago
 
         pago.importePagado = inscripcion.getPagADA_importe();
         pago.fechaPago = new Date();
-        pago.infoMedioPago = infoMedioPago; //Aca tenemos que poner el dato de tarjeta y no tiene que estar gardada en ningun lado.
-        pago.medioPago = medioPago; //Tarje
+        pago.infoMedioPago = infoMedioPago; // Aca tenemos que poner el dato de tarjeta y no tiene que estar gardada en
+                                            // ningun lado.
+        pago.medioPago = medioPago; // Tarje
         ResultadoPago rd = pagarServicio(inscripcion.getPagADA_ServicioId(), pago);
 
         if (rd.isOk) {
@@ -153,23 +164,23 @@ public class PagADAService {
 
         JsonNode r;
 
-        //Transformo el pago, como String en formato JSON
+        // Transformo el pago, como String en formato JSON
         String infoPagoStr = JSONObject.valueToString(pago);
 
-        //Levanto el string y lo leo en un objeto JSON
+        // Levanto el string y lo leo en un objeto JSON
         JSONObject infoPagoJson = new JSONObject(infoPagoStr);
 
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        //MODIFICO el nodo "fechaPago" a formato YYYY-MM-DD
+        // MODIFICO el nodo "fechaPago" a formato YYYY-MM-DD
         infoPagoJson.put("fechaPago", formatter.format(pago.fechaPago));
 
-        HttpResponse<JsonNode> request = Unirest.post("https://pagada.herokuapp.com/api/servicios/{id}")
-                .routeParam("id", servicioId.toString()).header("content-type", "application/json")
-                .body(infoPagoJson) //AcaPasamos el RequestBody
-                //una fecha la transforma asi: Tuesday 29 of September 2020
-                //lo tiene que mandar en YYYY-MM-DD
-                .header("api", "831DYEY1811NOMECORTENELSERVICIO").asJson(); //QUE TAMPOCO HAY QUE HAARCODEARLA
-
+        HttpResponse<JsonNode> request = Unirest.post(this.apiBaseUri + "/servicios/{id}")
+                .routeParam("id", servicioId.toString()).header("content-type", "application/json").body(infoPagoJson) // AcaPasamos
+                                                                                                                       // el
+                                                                                                                       // RequestBody
+                // una fecha la transforma asi: Tuesday 29 of September 2020
+                // lo tiene que mandar en YYYY-MM-DD
+                .header("api", this.apiKey).asJson(); // QUE TAMPOCO HAY QUE HAARCODEARLA
         r = request.getBody();
 
         JSONObject jsonObject = r.getObject();
